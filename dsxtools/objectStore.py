be7bb598eabf5f82dfea8341d149bd5ab13819ca
Greg Filla <gfilla23@gmail.com>
2017-04-07 20:38:28 -0500

    last try dependencies

--- dsxtools/objectStore.py
+++ dsxtools/objectStore.py
@@ -8,7 +8,10 @@ class objectStore:
 
     def get_string(self, fileName, return_values = True):
         """Get's a file from the object storage container for the credentials used to create the OS object.  Returns as a ByteIO Object"""
-
+        import requests
+        import json
+        from io import BytesIO
+        import pandas as pd
         credentials = self.credentials
         url1 = ''.join(['https://identity.open.softlayer.com', '/v3/auth/tokens'])
         data = {'auth': {'identity': {'methods': ['password'],
@@ -32,7 +35,10 @@ class objectStore:
 
     def get_csv(self, fileNamex):
         """Get's a file from the object storage container for the credentials used to create the OS object.  Returns as a ByteIO Object"""
-
+        import requests
+        import json
+        from io import BytesIO
+        import pandas as pd
         credentials = self.credentials
         url1 = ''.join(['https://identity.open.softlayer.com', '/v3/auth/tokens'])
         data = {'auth': {'identity': {'methods': ['password'],
@@ -56,7 +62,10 @@ class objectStore:
 
     def put_csv(self, fileName, fname = None):
         """Puts a csv file in object storage with default same name as existing file. The file needs to be saved to the local file system before using this method """
-
+        import requests
+        import json
+        from io import BytesIO
+        import pandas as pd
         credentials = self.credentials
 
         if not fname: #handling the name of the file
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(name='dsxtools',
       version='0.1',
